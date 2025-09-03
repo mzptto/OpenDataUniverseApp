d409dc27429987e0ae85bf0ae892911962a30f18
James <mzptto@amazon.co.uk>
2025-09-03 01:35:57 +0100

    Remove legacy root app folders (_legacy_src, _legacy_public)

--- src/utils/referenceDataLoader.js
+++
@@ -1,3 +0,0 @@
-// This file has been replaced by the generated referenceData.js
-// The reference data is now pre-loaded and available as a static import
-export { ReferenceDataManager } from '../data/referenceData';
